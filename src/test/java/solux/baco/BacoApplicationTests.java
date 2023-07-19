53bb4faca78c7218f5dd3b07f97997411544d45e
박혜민 <phm6318@sookmyung.ac.kr>
2023-07-19 22:49:41 +0900

    Initial commit

---
+++ src/test/java/solux/baco/BacoApplicationTests.java
@@ -0,0 +1,13 @@
+package solux.baco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
