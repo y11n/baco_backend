926a450eceacc00a3705bc53be2e49ceecc82953
phmhp <phm6318@sookmyung.ac.kr>
2023-07-28 03:55:00 +0900

    Feat: ReviewController 추가

--- src/main/java/solux/baco/controller/ReviewController.java
+++ src/main/java/solux/baco/controller/ReviewController.java
@@ -1,11 +1,14 @@
 package solux.baco.controller;
 
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import solux.baco.domain.Member;
+import solux.baco.service.MemberService;
 import solux.baco.service.ReviewModel.ReviewDTO;
 import solux.baco.service.ReviewService;
 import org.springframework.http.HttpStatusCode;
@@ -16,29 +19,77 @@ import solux.baco.service.ReviewModel.ReviewDTO;
 import solux.baco.service.ReviewService;
 import solux.baco.service.RouteService;
 
+import java.util.Optional;
+
 @Slf4j
 @RestController
 @RequestMapping("/Review")
+
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final MemberService memberService;
 
     @Autowired
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, MemberService memberService) {
         this.reviewService = reviewService;
+        this.memberService = memberService;
+    }
+
+//이메일로 member_id을 얻기 위해 반환값 형태 보려고 한 테스트 메서드 => writeReviewController로 들어갈 예정..
+    @GetMapping("/findByEmail")
+    @ResponseBody
+    public void writer(@RequestParam String email){
+        log.info("checkLog:ReviewController - here");
+
+        //String email = "test@test.com";
+        Optional<Member> writerInfo = memberService.findByEmail(email);
+        //log.info("checkLog:ReviewController - writeReviewController called with wirter_id: {}",writerInfo);
+
+        if (writerInfo.isPresent()) {
+            //Optional 객체 속의 요소인 Member객체를 가져오기 위해 .get() //(null이 아닐 때만 get으로 가져올 수 있음.)
+            Member member = writerInfo.get();
+            Long member_id = member.getMember_id();
+            log.info("checkLog: ReviewController - writeReviewController called with member_id: {}", member_id);
+        } else {
+            //예외처리
+            log.info("checkLog: ReviewController - Member not found with email: {}", email);
+        }
     }
 
 
     //후기 저장(후기작성)
-    @PostMapping("/create")
-    public ResponseEntity<String> saveReview(@RequestParam double[] startPlace, @RequestParam double[] endPlace, @RequestBody ReviewDTO reviewData) { //요청바디와 데이터 매핑.
+    @PostMapping("/write")
+    @ResponseBody
+    public ResponseEntity<String> writeReviewController(@RequestHeader HttpSession session , @RequestBody ReviewDTO reviewData) { //요청바디와 데이터 매핑.
         try {
+            log.info("checkLog:ReviewController -why why why");
+
             //전달받은 데이터 예외처리
 
 
-            //정상적인 로직 가능한 경우에 실행되는 부분
-            //ReviewService 호출
-            reviewService.saveReview(startPlace,endPlace,reviewData);
+            /**정상적인 로직 가능한 경우에 실행되는 부분*/
+
+            //ReviewService 호출 => 기능 변경
+            //reviewService.saveReview(startPlace,endPlace,reviewData);
+
+            //세션을 통해서 회원 정보 조회
+            //세션에서 loginEmail이라는 속성을 통해 얻은 정보를 string으로 변환해서 loginEmail이라는 string 변수에 담아줌.
+            String loginEmail = (String) session.getAttribute("loginEmail");
+            //얻은 loginEmail로 회원고유식별자를 알아냄. => MemberService의 기능을 사용하려면 생성자 만들어야할 것같음.
+
+            //memberService의 findEmail메서드를 호출하고,
+            // memberService의 findEmail 메서드는 memberRepository의 findByEmail을 호출하고,
+            // memberRepository의 findByEmail메서드는 Member객체를 반환함. (Optional<Member>)
+
+
+            Optional<Member> writerOfMember = memberService.findByEmail(loginEmail);
+            log.info("checkLog:ReviewController - writeReviewController called with wirter_id: {}",writerOfMember);
+
+
+
+            //db저장 하는 service호출.
+
 
             //db저장 성공 시
             return ResponseEntity.status(HttpStatus.CREATED).body("후기가 저장됐습니다.");
--- src/main/java/solux/baco/service/ReviewService.java
+++ src/main/java/solux/baco/service/ReviewService.java
@@ -2,24 +2,24 @@
 package solux.baco.service;
 
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import solux.baco.service.ReviewModel.ReviewDTO;
 
 @Service
 @Slf4j
 public class ReviewService {
 
-    public void saveReview(double[] start, double[] end, ReviewDTO reviewData) {
+    public void saveReview(@RequestHeader HttpSession session , @RequestBody ReviewDTO reviewData) {
+        //수정된 기능)경로 좌표 반환 api 호출해서 경로좌표,(경로기준)출발좌표,(경로기준)도착좌표 받아오기=>저장할 데이터 준비 완료 => 기능 변경
 
-        //수정
-        //1. 전달받은 출발지 장소명, 도착지 장소명에 해당하는 좌표 찾기 => 미리 데이터 준비? or 카카오맵으로 가능?
 
-        //2.경로 좌표 반환 api 호출해서 경로좌표,(경로기준)출발좌표,(경로기준)도착좌표 받아오기=>저장할 데이터 준비 완료
+        //2.트랜잭션으로 Route테이블과 Review테이블 묶어서 저장되도록 함.
 
-        //3.트랜잭션으로 Route테이블과 Review테이블 묶어서 저장되도록 함.
-
-        //4.(성공/실패)응답 반환
+        //3.응답 반환
 
     }
 
